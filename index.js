b68cff2cb6d63bdde36b25502814c91c55377e72
Maia Donoga <medeea@rocketmail.com>
2018-01-03 19:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function johnLennonFacts(){
   const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var newFacts = [];
      var i = 0;
-     while (array.length < facts.length) {
-        array.push(facts[i] + "!!!");
+     while (newFacts.length < facts.length) {
+        newFacts.push(facts[i] + "!!!");
         i++;
       }
-     return array;
+     return newFacts;
  }
 
 function iLoveTheBeatles(num) {
