892704d05dffcd2d34c21a2608566072add6403d
Maia Donoga <medeea@rocketmail.com>
2018-01-03 19:27:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function theBeatlesPlay(musicians, instruments){
 
   function johnLennonFacts(facts) {
    var results = [];
-   var count = 0;
-   while (count < facts.length) {
-     results.push(facts[count] + "!!!");
-     count++;
+   var i = 0;
+   while (i < facts.length) {
+     results.push(facts[i] + "!!!");
+     i++;
    }
    return results;
  }
